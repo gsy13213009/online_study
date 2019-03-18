c9e5c5f081663c23b9fc310f271e117d0f12ca67
郭思义 <gsy@meitu.com>
2019-03-18 17:37:02 +0800

    [optimize] 创建表单功能

--- online_study/urls.py
+++ online_study/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from apps.message import views as message_veiws
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('form/', message_veiws.message_form)
 ]
